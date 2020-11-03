cb0f8591b73c4d2fbe7ea31a65c3afb44ea50bb5
Gavin <tianyaogavin@gmail.com>
2020-11-03 20:26:25 +0800

    add interval BurnBlockHeight

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const data_root_path = `${root}${process.argv[3] || process.argv[2]}`
 const use_txs = process.argv[2] === '-t'
 
 app.get('/minerList', async (req, res) => {
-  let result = await getMinerInfo()
+  console.log(req.query)
+  let result = await getMinerInfo(req.query)
   console.log("result:", result)
   res.send(result)
 })
