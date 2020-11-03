ba25ff76cf3d82632658e618831e9a82bd2f8683
GavinGao <tianyaogavin@gmail.com>
2020-11-03 20:28:40 +0800

    add cors

--- server.js
+++ server.js
@@ -9,16 +9,30 @@ const root = ''
 const data_root_path = `${root}${process.argv[3] || process.argv[2]}`
 const use_txs = process.argv[2] === '-t'
 
+
+
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  if (req.method == 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+})
+
 app.get('/minerList', async (req, res) => {
   let result = await getMinerInfo()
   console.log("result:", result)
   res.send(result)
 })
-
+/*
 setInterval(function(){
   heapdump.writeSnapshot('./' + Date.now() + '.heapsnapshot');
 }, 360000);
-
+*/
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
